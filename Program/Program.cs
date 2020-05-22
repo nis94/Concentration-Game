f11a867a800f889f525d8b7a0d289aadc533f635
nis94 <54451483+nis94@users.noreply.github.com>
2020-05-22 16:35:45 +0300

    Add files via upload

---
+++ Program/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //  Entry Point - Call the UI
+            System.Console.WriteLine("Press Enter To Exit...");
+            System.Console.ReadLine();
+        }
+    }
+}
+
+
+
+//    Liran - Player
+//- name + score + chosenCards[2] 
+//- c'tor + getters\setters + chooseCard/Quit + RandomChoose (for computer) 
+
+
+
+//Nir - Board + Card
+//print X 2, 
+
+
+
+//Both - Menager 
+//- turnNumber, player1, player2, board, (? gameStatus?)
+//- checkIfPair, updateBoard(according to the chosen pair),  nextRound(according to the current player), checkGameStatus--->UI
