68e5e9784e9e9e589e34942858ba9244bb5d7727
Nir Bar levav <nirolol1994@gmail.com>
2020-05-23 15:19:31 +0300

    Sync - Plan

--- Concentration/UI.cs
+++ Concentration/UI.cs
@@ -4,12 +4,12 @@ namespace Concentration
     class UI
     {
 
-             1) move board print
-            2) Menu
-            3) Enter names;
-               4) Get num of players
-            5) Get Demantions
-            6) Get (string) Cordinatesfrom 
+            // 1) move board print
+            //2) Menu
+            //3) Enter names;
+            //   4) Get num of players
+            //5) Get Demantions
+            //6) Get (string) Cordinatesfrom 
 
 
 
