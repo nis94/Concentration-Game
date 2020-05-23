7a3e56e60ab6c0a9a16ebff5c57b23085ae79546
Liran Bahar <liranba@mta.ac.il>
2020-05-23 15:22:28 +0300

    test 4

--- Concentration/GameManager.cs
+++ Concentration/GameManager.cs
@@ -8,5 +8,54 @@ namespace Concentration
 {
     class GameManager
     {
+
+
+
+
+        //Turn(player)
+        //    {
+        //    string chos1, chos2
+
+
+
+        //   if (conputer)
+        //    {
+        //        chos1 = RandChoose()
+        //    }
+        //   else
+        //    {
+        //        chos1 = UI.Choose();
+        //    }
+
+        //    parse(chos1); // --- shoud rerturn i\j
+        //    Board[i,j].isFound=true
+        //    printBoardStatus()
+
+        //        chos2 = Choose();
+        //        if(equal)
+        //        {
+
+        //        }
+        //        else
+        //        {
+        //            sleep
+        //        }
+            }
+
+              
+
+            isPair=CheckIfpair(chos1, chos2);
+
+            if(isPair)
+            {
+                //1) Player score ++
+                /
+            }
+            else
+            {
+
+            }
+
+            }
     }
 }
--- Concentration/UI.cs
+++ Concentration/UI.cs
@@ -3,5 +3,17 @@ namespace Concentration
 {
     class UI
     {
+
+            // 1) move board print
+            //2) Menu
+            //3) Enter names;
+            //   4) Get num of players
+            //5) Get Demantions
+            //6) Get (string) Cordinatesfrom 
+
+
+
+
+
     }
 }
